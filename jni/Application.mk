6031accb811f254594fd4dd1af169548cd3f8161
Hung Ly <hunghly@gmail.com>
2022-01-19 11:57:56 -0600

    initial commit

---
+++ jni/Application.mk
@@ -0,0 +1,5 @@
+APP_OPTIM := debug    # Build the target in debug mode. 
+APP_ABI := all # Define the target architecture to be ARM.
+APP_STL := c++_static
+APP_CPPFLAGS := -frtti -fexceptions    # This is the place you enable exception.
+APP_PLATFORM := android-19 # Define the target Android version of the native application.
