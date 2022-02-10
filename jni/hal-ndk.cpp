302c7b3b49c9c10296134a4740073c0949e13751
hunghly <hunghly@gmail.com>
2022-02-10 15:53:42 -0600

    need to finish load_hw

--- jni/hal-ndk.cpp
+++ jni/hal-ndk.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <hardware.h>
 #include "hal-ndk.h"
+#include <dlfcn.h>
 
 int main()
 {
+    const hw_module_t* MODULE;
+    const char* module_id = "audio";
+
     std::cout << "Hello World!" << std::endl;
+    load_hw(module_id,&MODULE);
     return 0;
 }
 
 int load_hw(const char* module_id, const struct hw_module_t** hw_module) {
 
     std::cout << "Executing LOAD_HW" << std::endl;
-    
+    std::cout << "Attempting to load libhardware" << std::endl;
+
+    void* lib_hardware = dlopen("/system/lib/libhardware.so", RTLD_NOW);
+    if (!lib_hardware) {
+        std::cout << "Error opening libhardware.so" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
