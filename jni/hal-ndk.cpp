04af7f441c5460730accd026c354f3a5ee9cc20d
hunghly <hunghly@gmail.com>
2022-02-18 12:46:18 -0600

    need to work on getting modules

--- jni/hal-ndk.cpp
+++ jni/hal-ndk.cpp
@@ -15,11 +15,12 @@ int main()
 
 int load_hw(const char* module_id, const struct hw_module_t** hw_module) {
 
-    std::cout << "Executing LOAD_HW" << std::endl;
     std::cout << "Attempting to load libhardware" << std::endl;
     const char* LIBHARDWARE_DIR = "/system/lib64/libhardware.so\0";
+    const char* HW_GET_MODULE = "hw_get_module";
 
-    std::cout << LIBHARDWARE_DIR << std::endl;
+    // declare ptr to function called hw_get_module_t with two params
+    typedef int (*hw_get_module_t) (const char*, const struct hw_module_t**); 
 
     void* lib_hardware = dlopen(LIBHARDWARE_DIR, RTLD_NOW);
 
@@ -30,7 +31,26 @@ int load_hw(const char* module_id, const struct hw_module_t** hw_module) {
         return 1;
     }
 
-    std::cout << "Libhardware loaded" << std::endl;
+    std::cout << "Libhardware loaded. Gathering Funcs" << std::endl;
+    hw_get_module_t hw_get_module = (hw_get_module_t) dlsym(lib_hardware, HW_GET_MODULE);
+
+    if (!hw_get_module) {
+        std::cout << "Error getting funcs" << std::endl;
+        dlclose(lib_hardware);
+        return 1;
+    }
+    std::cout << "Funcs gathered. gathering modules" << std::endl;
+
+    int status = hw_get_module(module_id, hw_module);
+
+    if (status != 0) {
+        std::cout << "Error getting module" << std::endl;
+        dlclose(lib_hardware);
+        return 1;
+    }
+
+    std::cout << "Complete getting module" << std::endl;
+    dlclose(lib_hardware);
 
     return 0;
 }
